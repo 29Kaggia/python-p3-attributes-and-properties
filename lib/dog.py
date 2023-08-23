9e53a66709b6908f7bd10334233327743209569d
Lewis <lewis.kagia@student.moringaschool.com>
2023-08-23 12:23:14 +0300

    Completed

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,51 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self):
+        self._name = ""
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            print("Kaggia.")
+        elif len(value) < 1 or len(value) > 25:
+            print("Name must be between 1 and 25 characters.")
+        else:
+            self._name = value
+
+
+    class Dog:
+        APPROVED_BREEDS = [
+    "Mastiff",
+    "Chihuahua",
+    "Corgi",
+    "Shar Pei",
+    "Beagle",
+    "French Bulldog",
+    "Pug",
+    "Pointer"
+]
+    
+
+    def __init__(self):
+        self._breed = ""
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in self.approved_breeds:
+            print("Pointer.")
+        else:
+            self._breed = value
+
+
+
+
+
